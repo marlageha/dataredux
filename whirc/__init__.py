18decf3c6c2b300ee2dc3604defc8cda8e4cac7e
kareemelbadry <kareem@kareems-mbp.wireless.yale.internal>
2014-05-30 16:24:18 -0400

    modified so importing whirc also import the whirc_sort() function.

--- whirc/__init__.py
+++ whirc/__init__.py
@@ -1,6 +1,7 @@
 from whirc_setup import whirc_setup
 from whirc_mkbias import whirc_mkbias
 from whirc_info import whirc_info
+from whirc_sort import whirc_sort
 from whirc_mkflat import whirc_mkflat
 __all__ = ['whirc_setup','whirc_mkbias','whirc_mkflat']
 
