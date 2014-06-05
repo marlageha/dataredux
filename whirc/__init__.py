3cbc733540e796f22d32a5c044eb29565390f228
kareemelbadry <kareem@kareems-mbp.wireless.yale.internal>
2014-06-05 12:30:08 -0400

    now obsolete

--- whirc/__init__.py
+++ whirc/__init__.py
@@ -5,8 +5,7 @@ from whirc_sort import whirc_sort
 from whirc_mkflat import whirc_mkflat
 from whirc_mkdark import whirc_mkdark
 from whirc_rmpupil import whirc_rmpupil
-from whirc_55500 import whirc_55500
-from whirc_121130 import whirc_121130 
 from whirc_55500s import whirc_55500s
-__all__ = ['whirc_setup','whirc_mkbias','whirc_mkflat','whirc_info','whirc_sort','whirc_mkdark','whirc_rmpupil','whirc_55500', 'whirc_121130','whirc_55500s']
+from whirc_mksky import whirc_mksky
+__all__ = ['whirc_setup','whirc_mkbias','whirc_mkflat','whirc_info','whirc_sort','whirc_mkdark','whirc_rmpupil','whirc_55500s','whirc_mksky']
 
