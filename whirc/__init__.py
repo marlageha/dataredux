0f83d967a66164bf9c03c647565827542243818d
kareemelbadry <kareem@kareems-mbp.wireless.yale.internal>
2014-06-09 01:42:37 -0400

    added whirc_align.py

--- whirc/__init__.py
+++ whirc/__init__.py
@@ -7,5 +7,7 @@ from whirc_mkdark import whirc_mkdark
 from whirc_rmpupil import whirc_rmpupil
 from whirc_55500s import whirc_55500s
 from whirc_mksky import whirc_mksky
-__all__ = ['whirc_setup','whirc_mkbias','whirc_mkflat','whirc_info','whirc_sort','whirc_mkdark','whirc_rmpupil','whirc_55500s','whirc_mksky']
+from whirc_reduce import whirc_reduce
+from whirc_align import whirc_align
+__all__ = ['whirc_setup','whirc_mkbias','whirc_mkflat','whirc_info','whirc_sort','whirc_mkdark','whirc_rmpupil','whirc_55500s','whirc_mksky','whirc_reduce','whirc_align']
 
