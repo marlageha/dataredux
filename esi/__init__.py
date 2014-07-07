692c9fb5d6a0cf26f617887f8d9343ea7526e250
kareemelbadry <kareem@kareems-mbp.wireless.yale.internal>
2014-07-07 14:32:25 -0400

    got rid of superfluous stuff

--- esi/__init__.py
+++ esi/__init__.py
@@ -3,8 +3,7 @@ from esi_mkbias import esi_mkbias
 from esi_mkflat import esi_mkflat
 from esi_trace_cen import esi_trace_cen
 from esi_traceflat import esi_traceflat
-from esi_normflat import esi_normflat
-from esi_getorders import esi_getorders
+
 __all__ = ['esi_setup','esi_mkbias','esi_mkflat','esi_traceflat',
-           'esi_trace_cen', 'esi_normflat']
+           ]
 
